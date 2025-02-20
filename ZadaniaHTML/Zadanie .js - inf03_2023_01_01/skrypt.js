a36867cdb0e8dc6810c1ede153ec9ab9ef607c82
Nicokox1212 <144319801+Nicokox1212@users.noreply.github.com>
2025-02-20 09:29:03 +0100

    trtrtr

--- ZadaniaHTML/Zadanie .js - inf03_2023_01_01/skrypt.js
+++ ZadaniaHTML/Zadanie .js - inf03_2023_01_01/skrypt.js
@@ -1,7 +1,7 @@
 
 function obliczPuszki() {
     let powierzchnia = parseFloat(document.getElementById('powierzchnia').value);
-    let wydajnoscPuszki = 3.5; // m² na jedną puszkę
+    let wydajnoscPuszki = 3.5; 
     let liczbaPuszek = Math.ceil(powierzchnia / wydajnoscPuszki);
     document.getElementById('wynik').textContent = "Liczba potrzebnych puszek: " + liczbaPuszek;
 }
